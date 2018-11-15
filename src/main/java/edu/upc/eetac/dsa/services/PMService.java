6dff4f2596d9497afe7f3467fe8ee50168818957
MarcBelmonte <marc.belmonte.santacruz@gmail.com>
2018-11-15 20:54:22 +0100

    Initial commit

---
+++ src/main/java/edu/upc/eetac/dsa/services/PMService.java
@@ -0,0 +1,25 @@
+package edu.upc.eetac.dsa.services;
+
+import edu.upc.eetac.dsa.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.apache.log4j.Logger;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.List;
+
+public class PMService {
+
+    final static Logger log = Logger.getLogger(PMService.class.getName());
+    private Interface pm;
+
+    public PMService() {
+    }
+}
+
---
+++ src/test/java/edu/upc/eetac/dsa/InterfaceImplTest.java
@@ -0,0 +1,13 @@
+package edu.upc.eetac.dsa;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class InterfaceImplTest {
+   
+}
